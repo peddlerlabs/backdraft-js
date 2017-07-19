c65db790631e35360e192a37035d30629c37624b
KhoaNguyen <khoanguyendnvn@gmail.com>
2017-07-19 14:04:08 +0700

    add support for block types

--- index.js
+++ index.js
@@ -12,16 +12,32 @@ function relevantStyles(offset, styleRanges) {
   return styles.map(function (style) {return style.style});
 }
 
-function buildMarkup(rawDraftContentState, markup) {
+function getListItemsWrapper(blocks, index, isOpenTag, listItemsWrappers) {
+    if (isOpenTag && (index === 0 || (blocks[index - 1].type !== blocks[index].type))) {
+        return listItemsWrappers[blocks[index].type][0];
+    } else if (!isOpenTag && (index === blocks.length - 1 || (blocks[index].type !== blocks[index + 1].type))) {
+        return listItemsWrappers[blocks[index].type][1];
+    }
+
+    return null;
+}
+
+function buildMarkup(rawDraftContentState, markup, listItemsWrappers) {
 
   var blocks = rawDraftContentState.blocks;
-  return blocks.map(function convertBlock(block) {
-
+  return blocks.map(function convertBlock(block, index) {
     var outputText = [];
     var styleStack = [];
     var text = block.text;
     var ranges = block.inlineStyleRanges;
+    var type = block.type;
 
+    if (markup[type]) {
+        if (listItemsWrappers && listItemsWrappers[type]) {
+            outputText.push(getListItemsWrapper(blocks, index, true, listItemsWrappers));
+        }
+        outputText.push(markup[type][0]);
+    }
     // loop over every char in this block's text
     for (var i = 0; i < text.length; i++) {
 
@@ -29,7 +45,7 @@ function buildMarkup(rawDraftContentState, markup) {
       // (regardless of whether there *is* a next char or not)
       var characterStyles = relevantStyles(i, ranges);
       var nextCharacterStyles = relevantStyles(i + 1, ranges);
-      
+
       // calculate styles to add and remove
       var stylesToAdd = _.difference(characterStyles, styleStack);
       var stylesToRemove = _.difference(characterStyles, nextCharacterStyles);
@@ -47,6 +63,14 @@ function buildMarkup(rawDraftContentState, markup) {
         var toRemove = styleStack.pop();
         outputText.push(markup[toRemove][1]);
       }
+    }
+
+    if (markup[type]) {
+        outputText.push(markup[type][1]);
+
+        if (listItemsWrappers && listItemsWrappers[type]) {
+            outputText.push(getListItemsWrapper(blocks, index, false, listItemsWrappers));
+        }
     }
 
     return outputText.join('');
